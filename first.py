0d3c4606f6757fc904e76334fe3cee6059c269d2
Chypas <r.a.ovcharenko@gmail.com>
2022-10-20 19:48:05 +0300

    Initial commit

---
+++ first.py
@@ -0,0 +1,9 @@
+onesa = 2.5
+twoesa = 2
+dic = onesa * twoesa
+print(dic)
+print(type(dic))
+
+name = str(input("Please input your name" ))
+domain = str(input("Please input your domain" ))
+print(name, domain, sep= "@")
---
+++ test.py
@@ -0,0 +1,9 @@
+print("Welcome to calculator!")
+tape = input("Change option: ")
+
+a = input("Enter number: ");
+b = input("Enter number: ");
+a = int(a)
+b = int(b)
+c = a*b
+print(c)
